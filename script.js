58fbbae54581d73e4b4f5c150240a4ce6fcb6e2b
ulankachingun <145523623+ulankachingun@users.noreply.github.com>
2024-02-03 09:10:33 +0000

    done

---
+++ script.js
@@ -0,0 +1,12 @@
+function fnv() {
+    var x = document.getElementById("fn").value;
+    document.getElementById("fno").innerHTML = x;
+}
+function lnv() {
+    var x = document.getElementById("ln").value;
+    document.getElementById("lno").innerHTML = x;
+}
+function emv() {
+    var x = document.getElementById("em").value;
+    document.getElementById("emo").innerHTML = x;
+}
